3f31e382a72e49a4dd6492a460fdc13370bb5118
wKoja <cfwilliam.ferreira@gmail.com>
2025-02-24 16:50:57 -0300

    fixing sign-in page

--- src/routes/(app)/sign-in/+page.svelte
+++ src/routes/(app)/sign-in/+page.svelte
@@ -1,20 +1,28 @@
 
 <script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
 </script>
 
 <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-  <h1 class="text-2xl font-bold text-gray-800">Add New TOTP Code</h1>
-  <form method="post" action="?/add" class="w-full max-w-sm mt-4">
-    <label class="block mb-2">
-      <span class="text-gray-700">TOTP Code</span>
-      <input name="totp_code" class="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none" />
-    </label>
-    <label class="block mb-2">
-      <span class="text-gray-700">Description (optional)</span>
-      <input name="description" class="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none" />
-    </label>
-    <button class="px-4 py-2 mt-4 font-semibold text-white bg-blue-500 w-full rounded hover:bg-blue-600">Add TOTP</button>
+  <h1 class="text-2xl font-bold text-gray-800">Login/Register</h1>
+  <form method="post" action="?/login" use:enhance class="w-full max-w-sm mt-4">
+  	<label class="block mb-2">
+  		<span class="text-gray-700">Username</span>
+  		<input name="username" class="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none" />
+  	</label>
+  	<label class="block mb-2">
+  		<span class="text-gray-700">Password</span>
+  		<input type="password" name="password" class="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none" />
+  	</label>
+  	<div class="flex justify-between mt-4">
+  	  <button class="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600">Login</button>
+  	  <button formaction="?/register" class="px-4 py-2 font-semibold text-white bg-green-500 rounded hover:bg-green-600">Register</button>
+  	</div>
   </form>
+  <p class="mt-4 text-red-500">{form?.message ?? ''}</p>
 </div>
       
 
