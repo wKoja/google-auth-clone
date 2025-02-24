64294e5538ed8c823058c65f41512c2b21623fba
wKoja <cfwilliam.ferreira@gmail.com>
2025-02-24 17:17:41 -0300

    better root page

--- src/routes/(app)/user/+page.svelte
+++ src/routes/(app)/user/+page.svelte
@@ -7,7 +7,6 @@
 
 <div class="flex min-h-screen flex-col items-center justify-center bg-gray-100">
 	<h1 class="text-2xl font-bold text-gray-800">Hi, {data.user.username}!</h1>
-	<p class="mt-2 text-gray-600">Your user ID is {data.user.id}.</p>
 	<a href="/totp" class="rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"
 		>2FA Codes</a
 	>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<a href="/sign-in">Login</a>
-<a href="/totp">TOTP</a>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+  <h1 class="text-3xl font-bold text-gray-800 mb-6">Welcome to Google Auth Clone</h1>
+  <div class="space-x-4">
+    <a href="/sign-in" class="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600">Login</a>
+  </div>
+</div>
+
+
